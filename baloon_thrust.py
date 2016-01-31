465c32779f16701c10c183a3be36de6ac34a0d8c
meetsitaram <meetsitaram@gmail.com>
2016-01-30 22:26:49 -0500

    theory v/s actual results
    Therotical lift capacity varied from 22 to 68 grams changing various
    parameters.
    Actual lift capacity was only 12grams (2 penny coins (3.1gram each),
    baloon and nozzle weighing close to weight of the 2 coins)
    
    Therotical Results:
    exhaust velocity: 81.7374597236  m/s
    specific impulse: 8.3320550177  sec
    mass of air in baloon: 7.58901883216  grams
    air flow rate : 2.71036386863  grams/msec
    baloon thrust : 221.538257548  mN
    lift capacity : 22.5829008714  grams

--- baloon_thrust.py
+++ baloon_thrust.py
@@ -6,58 +6,67 @@ Created on Sat Jan 30 15:59:52 2016
 """
 from math import pi, sqrt
 
+gma=1.4             # gamma of air - ratio of specific heats at const. press. to that a const. vol. at NTP
+R=8.3145            # universal gas constant in J/mol.K
+MolM=28.966/1000.   # molecular mass of air in g/mol
+T_A=298.             # 25 degrees room temperature in kelvin
+T_BLOWN_AIR=310.    # assuming this is same as body temperature 37c
+pc=107991.          # pressure inside chamber(baloon)  (810 mmhg) - https://www.youtube.com/watch?v=fwh-i0WB_bQ
+#pa=101325.          # air pressure101325 kpa
+pa=103991.          # air pressure (780 hhmg) in N/m2 as in video
+
+air_density = 1.225     #kg/m3
+expanded_dia = .228     # diameter of expanded baloon (9 inch - rough measurement) in m
+exit_dia = .013           # measured 13mm
+g = 9.81
+total_exhaust_time = 2.8      # 2.8 sec approx (measured using stopwatch)
+#m_b = 1.8/1000.      # mass of baloon 1.8 grams approx. pack of 100 11inch baloons weighs 6.4 ounce (180g)
+   
+pe=pa             # pressure at exit of nozzle same as atm. press. with optimum nozzle
+
+IDEAL_NOZZLE = True
+if IDEAL_NOZZLE:
+    pe = pa
+
 def get_exhaust_velocity() :
-    gma=1.3             # gamma of air - ratio of specific heats at const. press. to that a const. vol. at NTP
-    R=8.3145            # universal gas constant in J/mol.K
-    MolM=28.966/1000.   # molecular mass of air in g/mol
-    TK=298.             # 25 degrees room temperature in kelvin
-    pc=111990.          # pressure inside chamber(baloon)  (840 mmhg) - https://www.youtube.com/watch?v=fwh-i0WB_bQ
-    pe=101325.          # pressure at exit of nozzle (760 hhmg) in N/m2 or 101325 kpa
-    
-    
-    v_exhaust = sqrt( ((2.*gma/(gma-1))*R*TK/MolM)*(1- (pe/pc)**((gma-1)/gma) ) )
+
+    v_exhaust = sqrt( ((2.*gma/(gma-1))*R*T_BLOWN_AIR/MolM)*(1- (pe/pc)**((gma-1)/gma) ) )
     print 'exhaust velocity:', v_exhaust, ' m/s' # result value : 112 m/s
     
-    g = 9.81
     print 'specific impulse:', v_exhaust/g, ' sec' 
     
     return v_exhaust
     
 def get_mass_of_air_in_baloon():
-
-    p_a=101325.          # air pressure 101kpa
-    p_c=111990.          # pressure inside chamber(baloon)  (840 mmhg) - https://www.youtube.com/watch?v=fwh-i0WB_bQ
+    volume_c = (4./3.)*pi*(expanded_dia/2.)**3
+    volume_a = pc*volume_c*T_A/(pa*T_BLOWN_AIR)     # pv/T is const at given temp.
     
-    dia = .2794     # diameter of expanded baloon (11inch) in m
-    volume_c = (4./3.)*pi*(dia/2.)**3
-    volume_a = p_c*volume_c/p_a     # pv is const at given temp.
+    ma = air_density*volume_a    
     
-    density = 1.225     #kg/m3
-    
-    ma = density*volume_a    
-    
-    print 'mass of air :', ma*1000., ' grams'  # result 15 grams
+    print 'mass of air in baloon:', ma*1000., ' grams'  # result 7.5 grams
     return ma
     
     
 def get_baloon_thrust():
-#    m_b = 1.8/1000.      # mass of baloon 1.8 grams approx. pack of 100 11inch baloons weighs 6.4 ounce (180g)
     
-    ma = get_mass_of_air_in_baloon()
     
-    total_exhaust_time = 3.      # 3 sec approx (only by visual observation)
-    
-    mass_flow_rate = ma / total_exhaust_time
+    ae = pi*(exit_dia/2.)**2.
     
     exhaust_velocity = get_exhaust_velocity()
+
+    ma = get_mass_of_air_in_baloon()    
+    mass_flow_rate = ma / total_exhaust_time    
     
     print 'air flow rate :', mass_flow_rate*1000., ' grams/msec'
     
-    thrust = mass_flow_rate * exhaust_velocity
+    exhaust_thrust = mass_flow_rate * exhaust_velocity
+
+    
+    
+    thrust = exhaust_thrust  + pe*ae - pa*ae  # for now assuming pe = pa
     
     print 'baloon thrust :', thrust*1000., ' mN'
-    
-    g = 9.81
+
     print 'lift capacity :', thrust*1000./g, ' grams'
       
     
